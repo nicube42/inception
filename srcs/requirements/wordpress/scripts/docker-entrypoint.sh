4bc712c47550467df809fa0e64170f220a34c037
Nicolas Diamantis <ndiamant@c2r5s6.42lausanne.ch>
2023-12-20 10:45:19 +0100

    start workpress site config

--- srcs/requirements/wordpress/scripts/docker-entrypoint.sh
+++ srcs/requirements/wordpress/scripts/docker-entrypoint.sh
@@ -6,5 +6,18 @@ sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
 sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
 cp wp-config-sample.php wp-config.php
 
+sleep 1
+
+if ! wp core is-installed --path=/var/www/html --quiet; then
+    echo "Installing WordPress..."
+    wp core install \
+        --url="Inception" \
+        --title="Inception" \
+        --admin_user="user0" \
+        --admin_password="user0" \
+        --admin_email="user0@example.com" \
+        --path=/var/www/html \
+        --skip-email
+fi
 
 exec php-fpm81 -F
